77771eb0fbf44e475368b2ffdab28443b5b6ba33
wujinsen <2998714655@qq.com>
2022-08-10 17:59:49 +0800

    commit

---
+++ moli-server/src/main/java/com/moli/system/controller/LogController.java
@@ -0,0 +1,92 @@
+package com.moli.system.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.moli.common.core.MoliResult;
+import com.moli.common.domain.entity.SysLoginLog;
+import com.moli.common.domain.entity.SysOperationLog;
+import com.moli.common.page.PageReq;
+import com.moli.common.page.PageRes;
+import com.moli.system.mapper.SysLoginLogMapper;
+import com.moli.system.mapper.SysOperationLogMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Api(tags = "系统操作日志管理")
+public class LogController {
+
+    @Autowired
+    private SysLoginLogMapper sysLoginLogMapper;
+
+    @Autowired
+    private SysOperationLogMapper sysOperationLogMapper;
+
+    @PostMapping("/loginLogList")
+    @ApiOperation(value = "登录日志列表")
+    public MoliResult<PageRes<SysLoginLog>> loginLogList(@RequestBody PageReq<SysLoginLog> req) {
+        PageRes<SysLoginLog> result = new PageRes<>();
+
+        Page<SysLoginLog> page = new Page<>(req.getPageNum(), req.getPageSize());
+        LambdaQueryWrapper<SysLoginLog> lambdaQueryWrapper = new LambdaQueryWrapper();
+        if (req.getData() != null) {
+            if (StringUtils.isNotBlank(req.getData().getRealName())) {
+                lambdaQueryWrapper.eq(SysLoginLog::getRealName, req.getData().getRealName());
+            }
+            if (StringUtils.isNotBlank(req.getData().getTelephone())) {
+                lambdaQueryWrapper.eq(SysLoginLog::getTelephone, req.getData().getTelephone());
+            }
+            if (req.getData().getStatus() != null) {
+                lambdaQueryWrapper.eq(SysLoginLog::getStatus, req.getData().getStatus());
+            }
+
+        }
+        lambdaQueryWrapper.orderByDesc(SysLoginLog::getLoginTime);
+        sysLoginLogMapper.selectPage(page, lambdaQueryWrapper);
+        result.setList(page.getRecords());
+        result.setPageNum(req.getPageNum());
+        result.setPageSize(req.getPageSize());
+        result.setTotal((int) page.getTotal());
+        return MoliResult.success(result);
+
+    }
+
+    @PostMapping("/oepartionLogList")
+    @ApiOperation(value = "操作日志列表")
+    public MoliResult<PageRes<SysOperationLog>> oepartionLogList(@RequestBody PageReq<SysOperationLog> req) {
+
+        PageRes<SysOperationLog> result = new PageRes<>();
+
+        Page<SysOperationLog> page = new Page<>(req.getPageNum(), req.getPageSize());
+        LambdaQueryWrapper<SysOperationLog> lambdaQueryWrapper = new LambdaQueryWrapper();
+        if (req.getData() != null) {
+            if (StringUtils.isNotBlank(req.getData().getUserName())) {
+                lambdaQueryWrapper.eq(SysOperationLog::getUserName, req.getData().getUserName());
+            }
+            if (StringUtils.isNotBlank(req.getData().getTelephone())) {
+                lambdaQueryWrapper.eq(SysOperationLog::getTelephone, req.getData().getTelephone());
+            }
+            if (req.getData().getStatus() != null) {
+                lambdaQueryWrapper.eq(SysOperationLog::getStatus, req.getData().getStatus());
+            }
+            if (req.getData().getBusinessType() != null) {
+                lambdaQueryWrapper.eq(SysOperationLog::getBusinessType, req.getData().getBusinessType());
+            }
+        }
+        lambdaQueryWrapper.orderByDesc(SysOperationLog::getCreateTime);
+        sysOperationLogMapper.selectPage(page, lambdaQueryWrapper);
+        result.setList(page.getRecords());
+        result.setPageNum(req.getPageNum());
+        result.setPageSize(req.getPageSize());
+        result.setTotal((int) page.getTotal());
+        return MoliResult.success(result);
+    }
+
+}
---
+++ moli-server/src/main/java/com/moli/system/mapper/SysLoginLogMapper.java
@@ -0,0 +1,7 @@
+package com.moli.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.moli.common.domain.entity.SysLoginLog;
+
+public interface SysLoginLogMapper  extends BaseMapper<SysLoginLog> {
+}
